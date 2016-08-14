5d0ffd12d8853b149ca2f968b80bffd56337c9e1
Aodzip <aodzip@gmail.com>
2016-08-14 15:34:57 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo "[1/1]正在获取最新版PhQAgent"
+echo "[1/1]正在获取最新版PhQAgent\n"
 wget "https://raw.githubusercontent.com/PhQAgent/Release/master/PhQAgent.phar"
 wget "https://raw.githubusercontent.com/PhQAgent/PhQAgent/master/start.sh"
 if [ `getconf LONG_BIT` = "64" ]; then
-	echo -n "[2/2] Linux 64 位操作系统检测"
-    wget "https://raw.githubusercontent.com/PhQAgent/Binary/master/linux64.tar.gz" | tar -zx > /dev/null 2>&1
+	echo -n "[2/2] Linux 64 位操作系统检测\n"
+    wget "https://raw.githubusercontent.com/PhQAgent/Binary/master/linux64.tar.gz"
+    tar zxvf linux64.tar.gz
+    rm linux64.tar.gz
 else
-	echo -n "[2/2] Linux 32 位操作系统检测"
-    wget "https://raw.githubusercontent.com/PhQAgent/Binary/master/linux32.tar.gz" | tar -zx > /dev/null 2>&1
-fi
+	echo -n "[2/2] Linux 32 位操作系统检测\n"
+    wget "https://raw.githubusercontent.com/PhQAgent/Binary/master/linux32.tar.gz"
+    tar zxvf linux32.tar.gz
+    rm linux32.tar.gz
 chmod +x ./bin/php/bin/php
-chmod start.sh
+chmod +x start.sh
 echo "[3/3] 安装完成 . start.sh 启动PhQAgent"
